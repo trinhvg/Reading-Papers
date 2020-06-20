1c23385438c59cd3bef2d8c9728d4773c91b27af
Tim V <49041972+timmyvg@users.noreply.github.com>
2020-06-20 18:00:35 +0900

    Update Note.py

--- Note.py
+++ Note.py
@@ -8,5 +8,5 @@
 ## Blog
 # https://www.wouterbulten.nl/blog/tech/improve-prostate-cancer-diagnosis-panda-challenge/#fn:gl1
 
-Blog = 'comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning'
-print(https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215)
+Blog_name = 'comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning'
+print('https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215')
