17315ad8a68fa57c70473edbfa31519fb8b1c36f
Tim V <49041972+timmyvg@users.noreply.github.com>
2020-06-21 17:54:03 +0900

    Update Note.py

--- Note.py
+++ Note.py
@@ -1,12 +1,15 @@
-# how to download data from Kaggle
-# https://medium.com/@ankushchoubey/how-to-download-dataset-from-kaggle-7f700d7f9198
+Blog_name = how to download data from Kaggle
+'https://medium.com/@ankushchoubey/how-to-download-dataset-from-kaggle-7f700d7f9198'
 
-# https://github.com/qfettes/DeepRL-Tutorials
+'https://github.com/qfettes/DeepRL-Tutorials'
 
-# https://github.com/p-christ/Deep-Reinforcement-Learning-Algorithms-with-PyTorch
+'https://github.com/p-christ/Deep-Reinforcement-Learning-Algorithms-with-PyTorch'
 
-## Blog
-# https://www.wouterbulten.nl/blog/tech/improve-prostate-cancer-diagnosis-panda-challenge/#fn:gl1
+Blog
+'https://www.wouterbulten.nl/blog/tech/improve-prostate-cancer-diagnosis-panda-challenge/#fn:gl1'
 
 Blog_name = 'comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning'
 print('https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215')
+
+Blog_name = 'Difference between view, reshape, transpose and permute in PyTorch'
+link = 'https://jdhao.github.io/2019/07/10/pytorch_view_reshape_transpose_permute/'
