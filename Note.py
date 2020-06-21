08e00c89945e01d6ddeada1e42756cff2dd31720
Tim V <49041972+timmyvg@users.noreply.github.com>
2020-06-21 17:55:23 +0900

    Update Note.py

--- Note.py
+++ Note.py
@@ -1,15 +1,15 @@
 Blog_name = how to download data from Kaggle
-'https://medium.com/@ankushchoubey/how-to-download-dataset-from-kaggle-7f700d7f9198'
+Link = 'https://medium.com/@ankushchoubey/how-to-download-dataset-from-kaggle-7f700d7f9198'
 
-'https://github.com/qfettes/DeepRL-Tutorials'
+Link = 'https://github.com/qfettes/DeepRL-Tutorials'
 
-'https://github.com/p-christ/Deep-Reinforcement-Learning-Algorithms-with-PyTorch'
+Link = 'https://github.com/p-christ/Deep-Reinforcement-Learning-Algorithms-with-PyTorch'
 
 Blog
-'https://www.wouterbulten.nl/blog/tech/improve-prostate-cancer-diagnosis-panda-challenge/#fn:gl1'
+link = 'https://www.wouterbulten.nl/blog/tech/improve-prostate-cancer-diagnosis-panda-challenge/#fn:gl1'
 
 Blog_name = 'comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning'
-print('https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215')
+Link = 'https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215'
 
 Blog_name = 'Difference between view, reshape, transpose and permute in PyTorch'
 link = 'https://jdhao.github.io/2019/07/10/pytorch_view_reshape_transpose_permute/'
